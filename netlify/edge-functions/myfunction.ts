c701241582dc4beb728d899edcd315c38a5d01d8
Paul McClean <wutangpaul@gmail.com>
2023-10-08 21:01:14 +0100

    Improve URL parser

--- netlify/edge-functions/myfunction.ts
+++ netlify/edge-functions/myfunction.ts
@@ -8,8 +8,28 @@ export default async (request: Request, context: Context) => {
   const response = await context.next()
 
   if (requestType === "document") {
-    console.log("request type is document")
-    response.headers.set("X-Your-Custom-Header", "A custom value")
+    //console.log("request type is document")
+    //console.log(context)
+    //console.log(Netlify.env.get("URL"))
+    //console.log(Netlify.env.get("CONTEXT"))
+    //console.log(request)
+    //response.headers.set("X-Your-Custom-Header", "A custom value")
+
+    if (
+      request.url.includes("localhost") ||
+      request.url.includes("netlify.app")
+    ) {
+      // parse domain string, return full domain
+      const url = new URL(request.url)
+      const domain = url.hostname
+      console.log("domain string: " + domain)
+    } else {
+      // remove all subdomains
+      const url = new URL(request.url)
+      const domain = url.hostname.replace(/^([^\.]+)\./, "")
+      console.log("domain string: " + domain)
+    }
+
     return response
   }
 
